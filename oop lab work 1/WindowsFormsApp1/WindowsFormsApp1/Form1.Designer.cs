dfe0cd521b3ab7d36059149b5e90b8a14ddca9eb
Дарина <131813220+dasmik0@users.noreply.github.com>
2025-09-09 18:10:19 +0300

    add project
    додано проект

---
+++ oop lab work 1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMass = new System.Windows.Forms.TextBox();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.txtSatellites = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Location = new System.Drawing.Point(29, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Назва планети:";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtName.Location = new System.Drawing.Point(172, 152);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtMass
+            // 
+            this.txtMass.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtMass.Location = new System.Drawing.Point(172, 198);
+            this.txtMass.Name = "txtMass";
+            this.txtMass.Size = new System.Drawing.Size(100, 20);
+            this.txtMass.TabIndex = 2;
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtDiameter.Location = new System.Drawing.Point(172, 239);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(100, 20);
+            this.txtDiameter.TabIndex = 3;
+            // 
+            // txtSatellites
+            // 
+            this.txtSatellites.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtSatellites.Location = new System.Drawing.Point(172, 285);
+            this.txtSatellites.Name = "txtSatellites";
+            this.txtSatellites.Size = new System.Drawing.Size(100, 20);
+            this.txtSatellites.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(29, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Маса (кг):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label3.Location = new System.Drawing.Point(29, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Діаметр (км):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label4.Location = new System.Drawing.Point(29, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Кількість супутників:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.kosmos_zhәne_zher_klimaty_turaly_35_қyzyқty_fakt;
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.Location = new System.Drawing.Point(330, 352);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 46);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Додати";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(315, 139);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(450, 186);
+            this.listBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Yest_li_zvuk_v_kosmose_;
+            this.ClientSize = new System.Drawing.Size(808, 468);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSatellites);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.txtMass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMass;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.TextBox txtSatellites;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ oop lab work 1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // список, у якому зберігатимуться всі планети
+        private List<Planet> planets = new List<Planet>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            double mass, diameter;
+            int satellites;
+
+            // Перевірка введених чисел
+            if (!double.TryParse(txtMass.Text, out mass))
+            {
+                MessageBox.Show("Некоректне значення для маси!");
+                return;
+            }
+
+            if (!double.TryParse(txtDiameter.Text, out diameter))
+            {
+                MessageBox.Show("Некоректне значення для діаметра!");
+                return;
+            }
+
+            if (!int.TryParse(txtSatellites.Text, out satellites))
+            {
+                MessageBox.Show("Некоректне значення для кількості супутників!");
+                return;
+            }
+
+            // Створення планети
+            Planet p = new Planet(name, mass, diameter, satellites);
+
+            planets.Add(p);
+            listBox1.Items.Add(p.ToString());
+
+            // Очищення полів
+            txtName.Clear();
+            txtMass.Clear();
+            txtDiameter.Clear();
+            txtSatellites.Clear();
+        }
+    }
+    // Клас Planet з властивостями
+    public class Planet
+    {
+        public string Name { get; set; }      // назва планети
+        public double Mass { get; set; }      // маса (кг)
+        public double Diameter { get; set; }  // діаметр (км)
+        public int Satellites { get; set; }   // кількість супутників
+
+        // Конструктор (задає значення при створенні об’єкта)
+        public Planet(string name, double mass, double diameter, int satellites)
+        {
+            Name = name;
+            Mass = mass;
+            Diameter = diameter;
+            Satellites = satellites;
+        }
+
+        // Перевизначений метод ToString() – як планета буде відображатися у списку
+        public override string ToString()
+        {
+            return $"{Name} | Маса: {Mass} кг | Діаметр: {Diameter} км | Супутники: {Satellites}";
+        }
+    }
+}
